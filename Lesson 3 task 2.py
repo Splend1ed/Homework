237431300e159e50318cdc191f0e4e518f628d1e
Andrei Korotenko <82520549+Splend1ed@users.noreply.github.com>
2021-04-20 20:07:30 +0300

    Update Lesson 3 task 2.py

--- Lesson 3 task 2.py
+++ Lesson 3 task 2.py
@@ -1,6 +1,7 @@
 #Checking the correctness of the number entry
 phone_number = (input("Enter the phone number:"))
-if len(phone_number) == 10:
-    print("All good!")
-elif len(phone_number) != 10:
-    print("Something went wrong.")
+while True:
+    if len(phone_number) == 10:
+        print("All good!")
+    elif len(phone_number) != 10:
+        print(input("Something went wrong.\n"))
