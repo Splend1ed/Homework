41aa221c9d230485168fb6217036acad90489fe0
Andrei Korotenko <82520549+Splend1ed@users.noreply.github.com>
2021-04-20 19:44:36 +0300

    Update Lesson 3 task 2.py

--- Lesson 3 task 2.py
+++ Lesson 3 task 2.py
@@ -1,5 +1,5 @@
 #Checking the correctness of the number entry
-phone_number = str(input("Enter the phone number:"))
+phone_number = (input("Enter the phone number:"))
 if len(phone_number) == 10:
     print("All good!")
 elif len(phone_number) != 10:
