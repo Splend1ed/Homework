a54721e33972ef7ccc79971c354fdba58d6cb50f
Splend1ed <korotenko.qndroid@gmail.com>
2021-04-21 23:00:54 +0300

    Homework Beetroot academy(unfinished task3 and task4)

---
+++ Lesson 4/Lesson 4 task 4.py
@@ -0,0 +1,12 @@
+tasks = input("Enter your task: ")
+answer = input("Enter your answer: ")
+sum_task = sum(tasks)
+while True:
+    if sum_task == answer:
+        print("All good")
+        break
+    else:
+        print("You so bad in math")
+        break
+
+# unfinished
