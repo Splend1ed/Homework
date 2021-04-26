67ad6025463ca0749b8a33f70f295e91d5597a7c
Andrei Korotenko <82520549+Splend1ed@users.noreply.github.com>
2021-04-27 00:54:04 +0300

    Update Lesson 6 task 1.py

--- Lesson 6/Lesson 6 task 1.py
+++ Lesson 6/Lesson 6 task 1.py
@@ -13,27 +13,3 @@ while True:
     else:
         print("Invalid error.\n")
     break
-
-
-
-
-
-
-
-
-# print("Welcome to the vocabulary auto maker")
-# while True:
-#     keys = input("Enter your keys: ")
-#     value = input("Enter your values: ")
-#     dictionary = {keys: value}
-#     print(dictionary)
-#     command_quit_or_restart = input('If you want to go out write "q" or if you want continue write "y"\n')
-#     if command_quit_or_restart == "q":
-#         break
-#     elif command_quit_or_restart == "y":
-#         dictionary.update({keys: value})
-#     else:
-#         print("Invalid error.\n")
-#     continue
-
-
