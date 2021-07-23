d256b8c3ce4eeebd78b8a5ef656daec23276afd7
Andrei Korotenko <82520549+Splend1ed@users.noreply.github.com>
2021-07-23 19:38:21 +0300

    Update Lesson_17_task_1(Test_Lesson_14_task_1).py

--- Lesson_17/Lesson_17_task_1(Test_Lesson_14_task_1).py
+++ Lesson_17/Lesson_17_task_1(Test_Lesson_14_task_1).py
@@ -1,13 +1,13 @@
 import unittest
-import Lesson_14_task_1
+from Lesson_14_task_1 import *
 
 
 class MyTestCase(unittest.TestCase):
     def test_add_func(self):
-        self.assertEqual(Lesson_14_task_1.add(1, 2), 3)
+        self.assertEqual(add(1, 2), 3)
 
     def test_square_all_func(self):
-        self.assertEqual(Lesson_14_task_1.square_all(2, 3), [4, 9])
+        self.assertEqual(square_all(2, 3), [4, 9])
 
 
 if __name__ == '__main__':
